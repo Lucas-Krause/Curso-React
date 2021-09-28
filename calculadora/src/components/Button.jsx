9b22abb46a49a1067c48ad034f71366f93c10958
Lucas-Krause <lucasbnekrause@gmail.com>
2021-09-28 12:54:40 -0300

    .

---
+++ calculadora/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.css';
+
+export default function Button(props) {
+	let classes = 'button ';
+	classes += props.operation ? 'operation' : '';
+	classes += props.double ? 'double' : '';
+	classes += props.triple ? 'triple' : '';
+	return <button className={classes} onClick={e => props.click && props.click(props.label)}>{props.label}</button>;
+}
---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,106 @@
+import { Component } from 'react';
+import Button from '../components/Button';
+import Display from '../components/Display';
+import './Calculator.css';
+
+const initialState = {
+	displayValue: '0',
+	clearDisplay: false,
+	operation: null,
+	values: [ 0, 0 ],
+	current: 0
+};
+
+export default class Calculator extends Component {
+	state = { ...initialState };
+
+	clearMemory() {
+		this.setState({ ...initialState });
+	}
+
+	setOperation(operation) {
+		if (this.state.current === 0) {
+			this.setState({ operation, current: 1, clearDisplay: true });
+		} else {
+			const equals = operation === '=';
+			const currentOperation = this.state.operation;
+
+			const values = [ ...this.state.values ];
+			switch (currentOperation) {
+				case '/':
+					values[0] = values[0] / values[1];
+					values[1] = 0;
+					break;
+				case '*':
+					values[0] = values[0] * values[1];
+					values[1] = 0;
+					break;
+				case '-':
+					values[0] = values[0] - values[1];
+					values[1] = 0;
+					break;
+				case '+':
+					values[0] = values[0] + values[1];
+					values[1] = 0;
+					break;
+				default:
+                    values[0] = `${values[0]} ${currentOperation} ${values[1]}`
+					break;
+			}
+
+			this.setState({
+				values,
+				displayValue: values[0],
+				operation: equals ? null : operation,
+				current: equals ? 0 : 1,
+                clearDisplay: !equals
+			});
+		}
+	}
+
+	addDigit(n) {
+		if (n === '.' && this.state.displayValue.includes('.')) {
+			return;
+		}
+
+		const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+		const currentValue = clearDisplay ? '' : this.state.displayValue;
+		const displayValue = currentValue + n;
+		this.setState({ displayValue, clearDisplay: false });
+
+		if (n !== '.') {
+			const i = this.state.current;
+			const newValue = parseFloat(displayValue);
+			const values = [ ...this.state.values ];
+			values[i] = newValue;
+			this.setState({ values });
+		}
+	}
+
+	render() {
+		const setOperation = (op) => this.setOperation(op);
+		const addDigit = (n) => this.addDigit(n);
+		return (
+			<div className="calculator">
+				<Display value={this.state.displayValue} />
+				<Button label="AC" triple click={() => this.clearMemory()} />
+				<Button label="/" click={setOperation} operation />
+				<Button label="7" click={addDigit} />
+				<Button label="8" click={addDigit} />
+				<Button label="9" click={addDigit} />
+				<Button label="*" click={setOperation} operation />
+				<Button label="4" click={addDigit} />
+				<Button label="5" click={addDigit} />
+				<Button label="6" click={addDigit} />
+				<Button label="-" click={setOperation} operation />
+				<Button label="1" click={addDigit} />
+				<Button label="2" click={addDigit} />
+				<Button label="3" click={addDigit} />
+				<Button label="+" click={setOperation} operation />
+				<Button label="0" click={addDigit} double />
+				<Button label="." click={addDigit} />
+				<Button label="=" click={setOperation} operation />
+			</div>
+		);
+	}
+}
